0f51735684ce41a9b1a33b3b316cec3979d42d70
montaquila <akiel@akiel.org>
2023-02-10 21:56:02 -0700

    adding blocking tests

---
+++ README.md
@@ -0,0 +1,10 @@
+# Exploring Hardware Verification
+This repo serves as a test/sandbox for working with hardware specific
+languages while testing some of my own electronic creations of 
+varying complexity. 
+
+Most of the work will be done on the hardware side meaning schematics
+will also be available. The simulations/verifications aim to provide
+insight and serve as a learning process to integrate some of my own
+circuit creations inspired from EE/CE.
+
---
+++ ex/ex1.v
@@ -0,0 +1,21 @@
+module block();
+reg a, b, c, d, e, f;
+// Blocking assignments
+initial begin
+    $display("a b c d e f at time %0t", $time);
+    $monitor("%d %d %d %d %d %d at time %0t", a, b, c, d, e, f, $time);
+
+    // sim assigns 1 to a at time 10
+    a = #10 1'b1;
+    // assign 0 to b @ 30
+    b = #20 1'b0;
+    // assign 1 to c @ 70
+    c = #40 1'b1;
+    // assign 1 to d @ 120
+    d = #50 1'b0;
+    e = #55 1'b1; 
+    f = #60 1'b0;
+
+end
+endmodule
+
