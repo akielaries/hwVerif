de18a4a29bb92c026d442d0ebc76314a0b477f50
montaquila <akiel@akiel.org>
2023-02-16 01:18:55 -0700

    adding phony test bench

--- comp/sv/simple.sv
+++ comp/sv/simple.sv
@@ -1,10 +1,13 @@
+`timescale 1ns/1ps
+
 module simple_comparator(
-    input input_a,
-    input input_b,
-    output res
+    input logic input_a,
+    input logic input_b,
+    output logic res
     );
     
-    assign res = (~input_a & ~input_b) | (input_a & input_b);
+    // assign res = (~input_a & ~input_b) | (input_a & input_b);
+    assign res = (input_a == input_b) ? 1'b1 : 1'b0;
 
 endmodule
 
---
+++ comp/sv/simple_tb.sv
@@ -0,0 +1,38 @@
+`timescale 1ns/1ps
+
+module simple_comparator_tb;
+    reg input_a;
+    reg input_b;
+    wire res;
+
+    simple_comparator uut(
+        .input_a(input_a),
+        .input_b(input_b),
+        .res(res)
+        );
+
+    initial begin
+        
+        input_a = 0;
+        input_b = 0;
+
+        #20 input_b = 1;
+        #20 input_a = 1;
+            input_b = 0;
+        #20 input_b = 1;
+        #40;
+    end
+
+    initial begin
+        $monitor("%d, %d = %d \n", input_a, input_b, res);
+    end
+
+    initial begin
+        $dumpfile("simple_comparator.vcd");
+        $dumpvars(0, simple_comparator_tb);
+    end
+
+
+
+endmodule
+
